c0595bb018d42e30538874f02dc73db75b3a9587
LUISCARLOS JIMENEZ <luiscarlos.jimenez@utp.ac.pa>
2026-09-24 21:13:23 -0500

    Actualización

--- MySql/Form1.Designer.cs
+++ MySql/Form1.Designer.cs
@@ -31,11 +31,14 @@
             components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             panel1 = new Panel();
+            pictureBox2 = new PictureBox();
+            txtBusqueda = new TextBox();
+            lblBusqueda = new Label();
             lblFolio = new Label();
             lblNombre = new Label();
             lblPrecio = new Label();
             lblCantidad = new Label();
-            dataGridView1 = new DataGridView();
+            dgvProductos = new DataGridView();
             Folio = new DataGridViewTextBoxColumn();
             Nombre = new DataGridViewTextBoxColumn();
             Precio = new DataGridViewTextBoxColumn();
@@ -44,20 +47,17 @@
             txtNombre = new TextBox();
             txtPrecio = new TextBox();
             txtCantidad = new TextBox();
-            lblBusqueda = new Label();
-            txtBusqueda = new TextBox();
             imageList1 = new ImageList(components);
             lblImagen = new Label();
             pictureBox1 = new PictureBox();
-            pictureBox2 = new PictureBox();
             btnGuardar = new Button();
             btnModificar = new Button();
             btnLimpiar = new Button();
             btnBorrar = new Button();
             panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvProductos).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // panel1
@@ -66,56 +66,92 @@
             panel1.Controls.Add(pictureBox2);
             panel1.Controls.Add(txtBusqueda);
             panel1.Controls.Add(lblBusqueda);
-            panel1.Location = new Point(59, 289);
+            panel1.Location = new Point(32, 140);
+            panel1.Margin = new Padding(2, 1, 2, 1);
             panel1.Name = "panel1";
-            panel1.Size = new Size(978, 150);
+            panel1.Size = new Size(527, 65);
             panel1.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.buscar;
+            pictureBox2.Location = new Point(321, 20);
+            pictureBox2.Margin = new Padding(2, 1, 2, 1);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(32, 28);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 12;
+            pictureBox2.TabStop = false;
+            // 
+            // txtBusqueda
+            // 
+            txtBusqueda.Location = new Point(123, 25);
+            txtBusqueda.Margin = new Padding(2, 1, 2, 1);
+            txtBusqueda.Name = "txtBusqueda";
+            txtBusqueda.Size = new Size(176, 23);
+            txtBusqueda.TabIndex = 1;
+            txtBusqueda.TextChanged += txtBusqueda_textChanged;
+            // 
+            // lblBusqueda
+            // 
+            lblBusqueda.AutoSize = true;
+            lblBusqueda.Location = new Point(33, 25);
+            lblBusqueda.Margin = new Padding(2, 0, 2, 0);
+            lblBusqueda.Name = "lblBusqueda";
+            lblBusqueda.Size = new Size(59, 15);
+            lblBusqueda.TabIndex = 0;
+            lblBusqueda.Text = "Búsqueda";
             // 
             // lblFolio
             // 
             lblFolio.AutoSize = true;
-            lblFolio.Location = new Point(60, 68);
+            lblFolio.Location = new Point(32, 32);
+            lblFolio.Margin = new Padding(2, 0, 2, 0);
             lblFolio.Name = "lblFolio";
-            lblFolio.Size = new Size(66, 32);
+            lblFolio.Size = new Size(33, 15);
             lblFolio.TabIndex = 1;
             lblFolio.Text = "Folio";
             // 
             // lblNombre
             // 
             lblNombre.AutoSize = true;
-            lblNombre.Location = new Point(59, 123);
+            lblNombre.Location = new Point(32, 58);
+            lblNombre.Margin = new Padding(2, 0, 2, 0);
             lblNombre.Name = "lblNombre";
-            lblNombre.Size = new Size(102, 32);
+            lblNombre.Size = new Size(51, 15);
             lblNombre.TabIndex = 2;
             lblNombre.Text = "Nombre";
             // 
             // lblPrecio
             // 
             lblPrecio.AutoSize = true;
-            lblPrecio.Location = new Point(58, 172);
+            lblPrecio.Location = new Point(32, 86);
+            lblPrecio.Margin = new Padding(2, 0, 2, 0);
             lblPrecio.Name = "lblPrecio";
-            lblPrecio.Size = new Size(79, 32);
+            lblPrecio.Size = new Size(40, 15);
             lblPrecio.TabIndex = 3;
             lblPrecio.Text = "Precio";
             // 
             // lblCantidad
             // 
             lblCantidad.AutoSize = true;
-            lblCantidad.Location = new Point(58, 218);
+            lblCantidad.Location = new Point(31, 110);
+            lblCantidad.Margin = new Padding(2, 0, 2, 0);
             lblCantidad.Name = "lblCantidad";
-            lblCantidad.Size = new Size(109, 32);
+            lblCantidad.Size = new Size(55, 15);
             lblCantidad.TabIndex = 4;
             lblCantidad.Text = "Cantidad";
             // 
-            // dataGridView1
+            // dgvProductos
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Folio, Nombre, Precio, Cantidad });
-            dataGridView1.Location = new Point(107, 485);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 82;
-            dataGridView1.Size = new Size(886, 186);
-            dataGridView1.TabIndex = 5;
+            dgvProductos.Columns.AddRange(new DataGridViewColumn[] { Folio, Nombre, Precio, Cantidad });
+            dgvProductos.Location = new Point(74, 224);
+            dgvProductos.Margin = new Padding(2, 1, 2, 1);
+            dgvProductos.Name = "dgvProductos";
+            dgvProductos.RowHeadersWidth = 50;
+            dgvProductos.Size = new Size(423, 104);
+            dgvProductos.TabIndex = 5;
+            dgvProductos.CellClick += dgvProductos_CellClick;
             // 
             // Folio
             // 
@@ -147,47 +183,35 @@
             // 
             // txtFolio
             // 
-            txtFolio.Location = new Point(195, 62);
+            txtFolio.Location = new Point(105, 29);
+            txtFolio.Margin = new Padding(2, 1, 2, 1);
             txtFolio.Name = "txtFolio";
-            txtFolio.Size = new Size(254, 39);
+            txtFolio.Size = new Size(139, 23);
             txtFolio.TabIndex = 6;
             // 
             // txtNombre
             // 
-            txtNombre.Location = new Point(195, 123);
+            txtNombre.Location = new Point(105, 54);
+            txtNombre.Margin = new Padding(2, 1, 2, 1);
             txtNombre.Name = "txtNombre";
-            txtNombre.Size = new Size(254, 39);
+            txtNombre.Size = new Size(139, 23);
             txtNombre.TabIndex = 7;
             // 
             // txtPrecio
             // 
-            txtPrecio.Location = new Point(195, 172);
+            txtPrecio.Location = new Point(105, 79);
+            txtPrecio.Margin = new Padding(2, 1, 2, 1);
             txtPrecio.Name = "txtPrecio";
-            txtPrecio.Size = new Size(255, 39);
+            txtPrecio.Size = new Size(139, 23);
             txtPrecio.TabIndex = 8;
             // 
             // txtCantidad
             // 
-            txtCantidad.Location = new Point(195, 218);
+            txtCantidad.Location = new Point(104, 104);
+            txtCantidad.Margin = new Padding(2, 1, 2, 1);
             txtCantidad.Name = "txtCantidad";
-            txtCantidad.Size = new Size(258, 39);
+            txtCantidad.Size = new Size(141, 23);
             txtCantidad.TabIndex = 9;
-            // 
-            // lblBusqueda
-            // 
-            lblBusqueda.AutoSize = true;
-            lblBusqueda.Location = new Point(62, 54);
-            lblBusqueda.Name = "lblBusqueda";
-            lblBusqueda.Size = new Size(119, 32);
-            lblBusqueda.TabIndex = 0;
-            lblBusqueda.Text = "Búsqueda";
-            // 
-            // txtBusqueda
-            // 
-            txtBusqueda.Location = new Point(229, 54);
-            txtBusqueda.Name = "txtBusqueda";
-            txtBusqueda.Size = new Size(324, 39);
-            txtBusqueda.TabIndex = 1;
             // 
             // imageList1
             // 
@@ -202,85 +226,86 @@
             // lblImagen
             // 
             lblImagen.AutoSize = true;
-            lblImagen.Location = new Point(585, 48);
+            lblImagen.Location = new Point(315, 22);
+            lblImagen.Margin = new Padding(2, 0, 2, 0);
             lblImagen.Name = "lblImagen";
-            lblImagen.Size = new Size(99, 32);
+            lblImagen.Size = new Size(50, 15);
             lblImagen.TabIndex = 10;
             lblImagen.Text = "Imagen:";
             // 
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.galeria_de_imagenes;
-            pictureBox1.Location = new Point(770, 48);
+            pictureBox1.Location = new Point(415, 22);
+            pictureBox1.Margin = new Padding(2, 1, 2, 1);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(200, 200);
+            pictureBox1.Size = new Size(108, 108);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 11;
             pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            pictureBox2.Image = Properties.Resources.buscar;
-            pictureBox2.Location = new Point(597, 42);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(60, 60);
-            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox2.TabIndex = 12;
-            pictureBox2.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
             // 
             // btnGuardar
             // 
             btnGuardar.ImageIndex = 3;
             btnGuardar.ImageList = imageList1;
-            btnGuardar.Location = new Point(97, 744);
+            btnGuardar.Location = new Point(52, 349);
+            btnGuardar.Margin = new Padding(2, 1, 2, 1);
             btnGuardar.Name = "btnGuardar";
-            btnGuardar.Size = new Size(143, 49);
+            btnGuardar.Size = new Size(77, 23);
             btnGuardar.TabIndex = 12;
             btnGuardar.Text = "Guadar";
             btnGuardar.TextImageRelation = TextImageRelation.ImageBeforeText;
             btnGuardar.UseVisualStyleBackColor = true;
+            btnGuardar.Click += btnGuardar_Click;
             // 
             // btnModificar
             // 
             btnModificar.ImageIndex = 2;
             btnModificar.ImageList = imageList1;
-            btnModificar.Location = new Point(303, 744);
+            btnModificar.Location = new Point(163, 349);
+            btnModificar.Margin = new Padding(2, 1, 2, 1);
             btnModificar.Name = "btnModificar";
-            btnModificar.Size = new Size(150, 49);
+            btnModificar.Size = new Size(81, 23);
             btnModificar.TabIndex = 13;
             btnModificar.Text = "Modificar";
             btnModificar.TextImageRelation = TextImageRelation.ImageBeforeText;
             btnModificar.UseVisualStyleBackColor = true;
+            btnModificar.Click += btnModificar_Click;
             // 
             // btnLimpiar
             // 
             btnLimpiar.ImageIndex = 0;
             btnLimpiar.ImageList = imageList1;
-            btnLimpiar.Location = new Point(517, 744);
+            btnLimpiar.Location = new Point(278, 349);
+            btnLimpiar.Margin = new Padding(2, 1, 2, 1);
             btnLimpiar.Name = "btnLimpiar";
-            btnLimpiar.Size = new Size(150, 49);
+            btnLimpiar.Size = new Size(81, 23);
             btnLimpiar.TabIndex = 14;
             btnLimpiar.Text = "Limpiar";
             btnLimpiar.TextImageRelation = TextImageRelation.ImageBeforeText;
             btnLimpiar.UseVisualStyleBackColor = true;
+            btnLimpiar.Click += btnLimpiar_Click;
             // 
             // btnBorrar
             // 
             btnBorrar.ImageIndex = 1;
             btnBorrar.ImageList = imageList1;
-            btnBorrar.Location = new Point(742, 744);
+            btnBorrar.Location = new Point(400, 349);
+            btnBorrar.Margin = new Padding(2, 1, 2, 1);
             btnBorrar.Name = "btnBorrar";
-            btnBorrar.Size = new Size(150, 49);
+            btnBorrar.Size = new Size(81, 23);
             btnBorrar.TabIndex = 15;
             btnBorrar.Text = "Borrar";
             btnBorrar.TextImageRelation = TextImageRelation.ImageBeforeText;
             btnBorrar.UseVisualStyleBackColor = true;
+            btnBorrar.Click += btnBorrar_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1092, 838);
+            ClientSize = new Size(588, 393);
             Controls.Add(btnBorrar);
             Controls.Add(btnLimpiar);
             Controls.Add(btnModificar);
@@ -291,19 +316,20 @@
             Controls.Add(txtPrecio);
             Controls.Add(txtNombre);
             Controls.Add(txtFolio);
-            Controls.Add(dataGridView1);
+            Controls.Add(dgvProductos);
             Controls.Add(lblCantidad);
             Controls.Add(lblPrecio);
             Controls.Add(lblNombre);
             Controls.Add(lblFolio);
             Controls.Add(panel1);
+            Margin = new Padding(2, 1, 2, 1);
             Name = "Form1";
             Text = "Form1";
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvProductos).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -334,5 +360,6 @@
         private Button btnModificar;
         private Button btnLimpiar;
         private Button btnBorrar;
+        private DataGridView dgvProductos;
     }
 }
--- MySql/Form1.cs
+++ MySql/Form1.cs
@@ -1,10 +1,248 @@
+
+using System.Drawing.Imaging;
 namespace MySql
 {
     public partial class Form1 : Form
     {
+        //Crea Producto para Guardar los datos.
+        //private Producto myNuevoProducto = new Producto();
+        int idProducto = 0;
+        private List<Producto> listaProductos;
+        private Dictionary<string, object> myProducto = new Dictionary<string, object>();
         public Form1()
         {
             InitializeComponent();
+            listaProductos = new List<Producto>();
+        }
+        private void cargarProductos(string filtro = "")
+        {
+            dgvProductos.Rows.Clear();
+            dgvProductos.Refresh();
+            listaProductos = Conexion.GetProducto(filtro);
+            //Recorrido de la Lista de Productos
+            foreach (var prod in listaProductos)
+            {
+                Image img = null;
+                if (prod.Imagen != null && prod.Imagen.Length > 0)
+                {
+                    using (MemoryStream ms = new MemoryStream(prod.Imagen))
+                    {
+                        using (Bitmap bmp = new Bitmap(ms))
+                        {
+                            img = new Bitmap(bmp); // Esto clona la imagen y evita que falle
+                        }
+                    }
+                }
+                dgvProductos.Rows.Add(prod.Id, prod.Nombre, prod.Precio, prod.Cantidad, img);
+
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cargarProductos();
+        }
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (datosCorrectos())
+            {
+                return; //No hacemos nada -  se define en este punto, puedes crear un punto de interrupción
+            }
+
+            CargarDatosProductos();
+
+            if (Conexion.InsertSeguro("productos", myProducto))
+            {
+                MessageBox.Show("Producto guardado correctamente.");
+                // Aquí refrescas el gird volviendo a consultar la base de datos
+                cargarProductos();
+            }//Fin del insertSeguro
+        }
+
+        private void CargarDatosProductos()
+        {
+            myProducto.Clear();
+            myProducto["Cantidad"] = int.Parse(txtCantidad.Text.Trim());
+            myProducto["Nombre"] = txtNombre.Text.Trim();
+            myProducto["Precio"] = decimal.Parse(txtPrecio.Text.Trim());
+            myProducto["Imagen"] = ImageToByteArray(pictureBox1.Image);
+        }
+
+        private byte[] ImageToByteArray(Image? image)
+        {
+            if (image == null)
+                return null;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                //Guardamos la imagen en su formato original (RawFormat)
+                image.Save(ms, image.RawFormat);
+                return ms.ToArray();
+            }
+        }
+
+        private bool datosCorrectos()
+        {
+            if (string.IsNullOrWhiteSpace(txtNombre.Text))
+            {
+                MessageBox.Show("El nombre del producto es obligatorio.");
+                return true;
+            }
+            if (txtPrecio.Text.Trim().Equals(""))
+            {
+                MessageBox.Show("El precio es obligatorio.");
+                return true;
+            }
+            if (txtCantidad.Text.Trim().Equals(""))
+            {
+                MessageBox.Show("La cantidad es obligatoria.");
+                return true;
+            }
+            if (!decimal.TryParse(txtPrecio.Text, out decimal precio) || precio < 0)
+            {
+                MessageBox.Show("El precio debe ser un número válido y no negativo.");
+                return true;
+            }
+            if (!int.TryParse(txtCantidad.Text, out int cantidad) || cantidad < 0)
+            {
+                MessageBox.Show("La cantidad debe ser un número entero válido y no negativo.");
+                return true;
+            }
+            return false; // Todos los datos son correctos
+        }
+
+
+
+        private void txtBusqueda_textChanged(object sender, EventArgs e)
+        {
+            cargarProductos(txtBusqueda.Text.Trim());
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "Seleccionar imagen del producto";
+                openFileDialog.Filter = "Archivos de imagen|*.jpg;*.jpeg;*.png;*.bmp";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Carga la imagen seleccionada en el PictureBox y me ajusta su tamaño
+                    pictureBox1.Image = Image.FromFile(openFileDialog.FileName);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+            }
+        }
+
+        private void dgvProductos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow fila = dgvProductos.Rows[e.RowIndex];
+            idProducto = 0;
+            idProducto = Convert.ToInt32(fila.Cells[0].Value);
+            txtNombre.Text = fila.Cells[1].Value.ToString();
+            txtPrecio.Text = fila.Cells[2].Value.ToString();
+            txtCantidad.Text = fila.Cells[3].Value.ToString();
+
+            btnGuardar.Enabled = false;
+            btnModificar.Enabled = true;
+        }
+        private void ModificarDatosBD()
+        {
+            CargarDatosProductos();
+            //Asegurar que mi arreglo tiene los datos
+            //MessageBox.Show("el Nombre del Producto es: " + myProducto["Nombre"]);
+            // Actualiza el producto donde el id_producto sea igual a 5
+
+
+            MessageBox.Show("el id del producto e: " + idProducto);
+
+            bool resultado = Conexion.UpdateSeguro("productos", myProducto, "id", idProducto);
+
+            if (resultado)
+            {
+                Console.WriteLine("Actualización exitosa.");
+            }
+        }
+
+        private void limpiarFormulario()
+        {
+            txtFolio.Text = "";
+            txtNombre.Text = "";
+            txtPrecio.Text = "";
+            txtCantidad.Text = "";
+            txtBusqueda.Text = "";
+            pictureBox1.Image = null;
+            idProducto = 0;
+
+            btnGuardar.Enabled = true;
+            btnModificar.Enabled = false;
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            limpiarFormulario();
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            // Validar que exista una selección activa
+            if (idProducto == 0)
+            {
+                MessageBox.Show("Por favor, selecciona un producto de la lista para eliminar.");
+                return;
+            }
+
+            // Pedir confirmación al usuario antes de borrar
+            DialogResult confirmacion = MessageBox.Show(
+                "¿Estás seguro de que deseas eliminar este producto?",
+                "Confirmar eliminación",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning
+            );
+
+            if (confirmacion == DialogResult.Yes)
+            {
+                // Ejecutar la eliminación
+                if (Conexion.DeleteSeguro("productos", "id", idProducto))
+                {
+                    MessageBox.Show("Producto eliminado correctamente.");
+                    cargarProductos();      // Recargar la tabla
+                    limpiarFormulario();    // Resetear formulario
+                }
+                else
+                {
+                    MessageBox.Show("Ocurrió un error al intentar eliminar el producto.");
+                }
+            }
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            // Validar que haya un producto seleccionado
+            if (idProducto == 0)
+            {
+                MessageBox.Show("Por favor, selecciona un producto del listado para modificar.");
+                return;
+            }
+
+            // Validar que los campos de texto no tengan errores
+            if (datosCorrectos())
+            {
+                return;
+            }
+
+            // Cargar los datos actuales de las cajas de texto en la estructura del diccionario
+            CargarDatosProductos();
+
+            // Ejecutar la actualización en MySQL
+            if (Conexion.UpdateSeguro("productos", myProducto, "id", idProducto))
+            {
+                MessageBox.Show("Producto actualizado correctamente.");
+                cargarProductos();      // Recargar la tabla
+                limpiarFormulario();    // Resetear formulario
+            }
+            else
+            {
+                MessageBox.Show("Ocurrió un error al intentar modificar el producto.");
+            }
         }
     }
 }
